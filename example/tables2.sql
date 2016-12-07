b66be9021e3779618a7dca8e9ef059ed8f4154e9
marcochavarria <marco.chavarria@vitec.com>
2016-12-07 13:00:46 +0100

    Docker File for the postgres-database. Configuration files and examples

---
+++ example/tables2.sql
@@ -0,0 +1,23 @@
+
+CREATE EXTENSION cstore_fdw;
+
+
+CREATE SERVER cstore_server FOREIGN DATA WRAPPER cstore_fdw;
+
+CREATE FOREIGN TABLE customer_reviews
+(
+    customer_id TEXT,
+    review_date DATE,
+    review_rating INTEGER,
+    review_votes INTEGER,
+    review_helpful_votes INTEGER,
+    product_id CHAR(10),
+    product_title TEXT,
+    product_sales_rank BIGINT,
+    product_group TEXT,
+    product_category TEXT,
+    product_subcategory TEXT,
+    similar_product_ids CHAR(10)[]
+)
+SERVER cstore_server
+OPTIONS(compression 'pglz');
